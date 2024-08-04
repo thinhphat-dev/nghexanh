6952e31536b35ff14796fc4e526a75cfcd2b554f
thinhphat-dev <thinhphat.dev@gmail.com>
2024-08-05 02:15:15 +0700

    update(7)

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use('/api', router);
-server.listen(3000, () => {
-    console.log('JSON Server is running');
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`);
 });
