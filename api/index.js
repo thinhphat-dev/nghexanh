e83bfb1e2ec85c00f4eb900b413eed88dd85c970
thinhphat-dev <thinhphat.dev@gmail.com>
2024-08-05 10:29:02 +0700

    update(22)

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
-
 const express = require('express');
-const data = require('api/data');
+const data = require('./data'); // Đảm bảo rằng bạn đã tạo file data.js với dữ liệu tĩnh
 const app = express();
 const port = process.env.PORT || 3000;
 
